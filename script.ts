cad4566fa64b7495e7bef49426d33cb6b4a78f06
GRAUF <graufkhankhi@gmail.com>
2024-09-17 20:27:25 +0500

    first commit

---
+++ script.ts
@@ -0,0 +1,15 @@
+const toggleSkillsBtn1 = document.getElementById('toggle-skills-btn')!;
+
+const skillsSection1 = document.getElementById('skills')!;
+
+toggleSkillsBtn1.addEventListener('click', () => {
+  if (skillsSection1.style.display === 'none') {
+    skillsSection1.style.display = 'block';
+    toggleSkillsBtn1.textContent = 'Hide Skills';
+  } else {
+    skillsSection1.style.display = 'none';
+    toggleSkillsBtn1.textContent = 'Show Skills';
+  }
+});
+
+const toggleSkillsBtn2 = document.getElementById('toggle-skills-btn2')!;
